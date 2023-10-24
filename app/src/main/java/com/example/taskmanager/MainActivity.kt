ede8ec9d66a2270ce774c0912b856471e416ccca
Semi <a@a.com>
2023-10-24 15:59:18 +0200

    SuperFinish

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.taskmanager
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,20 +14,29 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.TextStyle
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,13 +44,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TaskManagerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                App()
             }
         }
     }
@@ -47,10 +53,38 @@ class MainActivity : ComponentActivity() {
 
 data class Task(val id: Int,val info: String, var complete: Boolean = false)
 
-val list = List(20) {Task(it + 1, "Tarea ${it + 1}", false)}
+val list = List(20) {Task(it + 1, "Tarea ${it + 1}", false)}.toMutableStateList()
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TaskManager(tasksList : List<Task>, onComplete : (Task) -> Unit, onDelete : (Task) -> Unit) {
+fun TaskManager(tasksList : List<Task>, onComplete : (Task) -> Unit, onDelete : (Task) -> Unit, onAdd : (String) -> Unit) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(14.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var newTaskText by remember { mutableStateOf("") }
+        TextField(
+            value = newTaskText,
+            onValueChange = { newTaskText = it },
+            placeholder = { Text("Introducir nueva tarea") }
+        )
+        Button(
+            onClick = {
+                if (newTaskText.isNotEmpty()) {
+                    onAdd(newTaskText)
+                    newTaskText = ""
+                }
+            },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text("Añadir Tarea")
+        }
+    }
+
     LazyColumn {
         items(tasksList) {
             task ->
@@ -77,8 +111,37 @@ fun TaskManager(tasksList : List<Task>, onComplete : (Task) -> Unit, onDelete : 
                 IconButton(onClick = {onDelete(task)}, modifier = Modifier.size(24.dp)) {
                     Icon(imageVector = Icons.Default.Delete, contentDescription = "Deleted", tint = MaterialTheme.colorScheme.primary)
                 }
-                
             }
+        }
+    }
+}
+
+
+@Composable
+fun App() {
+    val tasksList by remember { mutableStateOf(list) }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+        ) {
+
+            TaskManager(
+                tasksList = tasksList,
+                onComplete = { task ->
+                    task.complete = !task.complete
+                    },
+                onDelete = { task ->
+                    tasksList.remove(task)
+                },
+                onAdd = { newTaskText ->
+                    val newTask = Task(tasksList.size + 1, newTaskText, false)
+                    tasksList.add(newTask)
+                }
+            )
         }
     }
 }
